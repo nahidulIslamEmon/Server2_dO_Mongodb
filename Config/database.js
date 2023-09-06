f3cb64cd8c75e3f3a6138257e1aa614da831524d
MD. NAHIDUL ISLAM EMON <135589360+nahidulIslamEmon@users.noreply.github.com>
2023-09-06 09:28:03 +0600

    Update database.js

--- Config/database.js
+++ Config/database.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose
     .connect(
-      "mongodb+srv://dimvaji:dimvaji12345@cluster0.wmfkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+          "mongodb+srv://dimvaji1234:WP6jMYYOyYrLAzEr@dimvaji.wrasz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
     )
     .then((data) => {
       console.log(`Database Connected To ${data.connection.host}`);
